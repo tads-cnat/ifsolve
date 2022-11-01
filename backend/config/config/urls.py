06b37f1e11c93aaaaf21e6829b831d3d58c934b6
Raphael <raphael@raphael.s-workspace>
2022-11-01 07:55:58 -0300

    Adcionando swagger e criando rota

--- backend/config/config/urls.py
+++ backend/config/config/urls.py
@@ -17,13 +17,24 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from ifsolve.views import ItemViewSet
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
 
 router = routers.DefaultRouter()
 router.register(r'aluno', ItemViewSet)
 
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="IFSolve API",
+        default_version='1.0.0',
+        description="API para o projeto IFSolve",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
