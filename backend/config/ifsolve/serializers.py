1777850aeb15982c946b244103cfe71b912338e7
Raphael <raphael@raphael.s-workspace>
2022-11-04 12:20:39 -0300

    Criando serializers de models do sistema #8

--- backend/config/ifsolve/serializers.py
+++ backend/config/ifsolve/serializers.py
@@ -1,6 +1,32 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import (Alternativa, Aluno, Area, Avaliacao, Elaborador, Item, ItemAvaliacao, Resposta, Tag, Usuario)
+from .models import (Alternativa, Aluno, Area, Avaliacao, Elaborador, Item, ItemAvaliacao, Resposta, Tag, Usuario, Resposta)
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = "__all__"
+
+class ElaboradorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Elaborador
+        fields = "__all__"
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = "__all__"
+
+class AreaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Area
+        fields = "__all__"
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
 
 class AlternativaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,3 +43,19 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = "__all__"
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Avaliacao
+        fields = "__all__"
+
+class ItemAvaliacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemAvaliacao
+        fields = "__all__"
+
+class RespostaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Resposta
+        fields = "__all__"
+    
