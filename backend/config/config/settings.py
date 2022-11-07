2b7dcd5d1bec5a76ed6670c262e9bd82b5b856cd
Raphael <raphael@raphael.s-workspace>
2022-11-07 13:42:59 -0300

    Configurando servidor postgres #21

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
