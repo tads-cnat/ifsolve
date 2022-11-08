85eb195518d5f4bfab9a9f9feea6790432ea0d03
Raphael <raphael@raphael.s-workspace>
2022-11-08 15:24:29 -0300

    Criando URL para documentação da API#34

--- backend/config/config/urls.py
+++ backend/config/config/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
