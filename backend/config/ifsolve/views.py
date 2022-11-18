8f79fbf3b597504489461f9eacbfff15e764abc1
Raphael <raphael@raphael.s-workspace>
2022-11-18 15:41:54 -0300

    Criando métodos de view de item

--- backend/config/ifsolve/views.py
+++ backend/config/ifsolve/views.py
@@ -14,20 +14,38 @@ class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.none()
     serializer_class = ItemSerializer
 
+    def retrieve(self, request, pk=None):
+        #Reescrevendo método de recuperação de um item
+        queryset = Item.objects.all()
+        item = get_object_or_404(queryset, pk=pk)
+        serializer = ItemSerializer(item)
+        return Response(serializer.data)
+
     # @action(detail=True, methods=['get'], permission_classes=[IsAluno])
-    @action(detail=True, methods=['get'], url_path='aluno/item')
-    def alunoitem(self, request, pk = None):
-        item = Item.objects.filter(visibilidade = 'PU')
+    @action(detail=False, methods=['get'], url_path='aluno')
+    def alunoItens(self, request, pk = None):
+        #View para o aluno ver todos os itens públicos
+        item = Item.objects.filter(visibilidade = 'PU') 
         serializer = ItemSerializer(item, many=True)
         return Response(serializer.data)
 
-    @action(detail=True, methods=['get'], url_path='elaborador/item')
-    def elaboradoritem(self, request, pk = None):
+    # @action(detail=True, methods=['get'], permission_classes=[IsElaborador])
+    @action(detail=False, methods=['get'], url_path='elaborador')
+    def elaboradorItens(self, request, pk = None):
+        # View para o elaborador ver os itens que ele criou
         item = Item.objects.filter(elaborador=request.user.usuario.elaborador)
         serializer = ItemSerializer(item, many=True)
         return Response(serializer.data)
-
-    # criar método de item para uma avaliação. 
+    
+    # @action(detail=False, methods=['post'], url_path='elaborador/criar', permission_classes=[IsElaborador])
+    @action(detail=False, methods=['post'], url_path='elaborador/criar')
+    def criarItem(self, request, pk = None):
+        # View para o elaborador criar um item
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(elaborador=request.user.usuario.elaborador)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
