5d482c3202fc8f5491f70516ae8dc93a4a86e4f6
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2022-11-28 18:08:53 -0300

    Finalizando autenticações

--- backend/config/ifsolve/permissions.py
+++ backend/config/ifsolve/permissions.py
@@ -15,3 +15,11 @@ class IsAluno(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_authenticated and hasattr(request.user.usuario, 'aluno')
+
+class AllowAny(permissions.BasePermission):
+    """
+    Permissão para qualquer usuário.
+    """
+
+    def has_permission(self, request, view):
+        return True
--- backend/config/ifsolve/views.py
+++ backend/config/ifsolve/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from .permissions import IsElaborador, IsAluno
+from .permissions import IsElaborador, IsAluno, AllowAny
 from .models import (Alternativa, Aluno, Area, Avaliacao, Elaborador, Item, ItemAvaliacao, Resposta, Tag, Usuario)
 from .serializers import (AlternativaSerializer, AlunoSerializer, AreaSerializer, AvaliacaoSerializer, ElaboradorSerializer, ItemSerializer, ItemAvaliacaoSerializer, RespostaSerializer, TagSerializer, UsuarioSerializer)
 
@@ -12,7 +12,7 @@ class CadastroAlunoViewSet(viewsets.ModelViewSet):
     queryset = Aluno.objects.none()
     serializer_class = AlunoSerializer
     
-    @action(detail=False, methods=['post'], url_path = "cadastro")
+    @action(detail=False, methods=['post'], url_path = "cadastro", permission_classes = [AllowAny])
     def post(self, request):
         serializer = AlunoSerializer(data = request.data)
         if serializer.is_valid():
@@ -24,7 +24,7 @@ class CadastroElaboradorViewSet(viewsets.ModelViewSet):
     queryset = Elaborador.objects.none()
     serializer_class = ElaboradorSerializer
     
-    @action(detail=False, methods=['post'], url_path = "cadastro")
+    @action(detail=False, methods=['post'], url_path = "cadastro", permission_classes = [AllowAny])
     def post(self, request):
         serializer = ElaboradorSerializer(data = request.data)
         if serializer.is_valid():
@@ -43,14 +43,14 @@ class ItemViewSet(viewsets.ModelViewSet):
         serializer = ItemSerializer(item)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['get'], permission_classes=[IsAluno], url_path='aluno')
+    @action(detail=False, methods=['get'], url_path='aluno', permission_classes=[IsAluno])
     def alunoItens(self, request, pk = None):
         #View para o aluno ver todos os itens públicos
         item = Item.objects.filter(visibilidade = 'PU') 
         serializer = ItemSerializer(item, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['get'], permission_classes=[IsElaborador], url_path='elaborador')
+    @action(detail=False, methods=['get'], url_path='elaborador', permission_classes=[IsElaborador])
     def elaboradorItens(self, request, pk = None):
         # View para o elaborador ver os itens que ele criou
         item = Item.objects.filter(elaborador=request.user.usuario.elaborador)
@@ -70,10 +70,18 @@ class RespostaItemViewSet(viewsets.ModelViewSet):
     queryset = Resposta.objects.none()
     serializer_class = RespostaSerializer
 
-    # @action(detail=True, methods=['get'], permission_classes=[IsAluno])
-    @action(detail=True, methods=['get'])
-    def resposta(self, request, pk=None):
-        item = get_object_or_404(Item, pk=pk)
+    @action(detail=False, methods=['get'], url_path='aluno/item/(?P<item_id>[^/.]+)', permission_classes=[IsAluno])
+    def resposta(self, request, pk=None, item_id=None):
+        item = get_object_or_404(Item, pk=item_id)
         respostas = Resposta.objects.filter(item=item, aluno=request.user.usuario.aluno)
         serializer = RespostaSerializer(respostas, many=True)
         return Response(serializer.data)
+
+    @action(detail=False, methods=['post'], url_path='aluno/responder/(?P<item_id>[^/.]+)', permission_classes=[IsAluno])
+    def responder(self, request, pk=None, item_id=None):
+        item = get_object_or_404(Item, pk=item_id)
+        serializer = RespostaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(item=item, aluno=request.user.usuario.aluno)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
