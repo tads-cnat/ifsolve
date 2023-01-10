cb3e08f5d05bc1d7e989c779bf1f52916899dbd8
diogoodiego <diogo.sam.nascimento@gmail.com>
2023-01-10 09:58:25 -0300

    Adicionada validação com yup no fomurlário de login (#75)

--- frontend/src/components/formControl/index.js
+++ frontend/src/components/formControl/index.js
@@ -5,7 +5,7 @@ export default function FormControl(props) {
             <Field
                 type={props.type ? props.type : "text"}
                 name={props.name}
-                className={"w-full rounded-lg px-4 py-2 border border-dark-10 focus:outline-5 focus:outline-primary-100 " + props.className}
+                className={"w-full rounded-lg px-4 py-2 border border-dark-10 hover:border-dark-20 hover:bg-dark-5 focus:outline-5 focus:outline-primary-100 focus:bg-primary-5 " + props.className}
                 placeholder={props.placeholder}
             />
         </div>
--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -6,19 +6,36 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 import IFSolvelogo from "../../images/IFSolve-logo.svg";
 import { useState } from "react";
 import { FiEye, FiEyeOff } from "react-icons/fi";
+import * as Yup from "yup";
 
 export default function Login() {
     const navigate = useNavigate();
     const [getPassword, setPassword] = useState(true);
+    const [getError, setError] = useState();
+
+    const LoginSchema = Yup.object().shape({
+        login: Yup.string().required(),
+        password: Yup.string().required(),
+    })
 
     const initialValues = {
+
         login: "",
         password: "",
     }
 
     async function formSubmit(data) {
-        await loginApi(data);
-        await navigate("/item");
+        await loginApi(data)
+            .then((res) => {
+                localStorage.setItem('ifsolve_token', res.data.token)
+                localStorage.setItem('ifsolve_user', JSON.stringify(res.data))
+                navigate("/item");
+            })
+            .catch((error) => {
+                if (error.response.status === 400) {
+                    setError("Usuário e/ou senha inválidos");
+                }
+            })
     }
 
     return (
@@ -27,14 +44,15 @@ export default function Login() {
             <div className="col-span-5 flex flex-col justify-center h-screen px-8 md:px-24">
                 <img className="h-6 mb-12 self-start" src={IFSolvelogo} alt="" />
 
-                <Formik initialValues={initialValues} onSubmit={formSubmit}>
-                    {({ values }) =>
+                <Formik initialValues={initialValues} validationSchema={LoginSchema} onSubmit={formSubmit}>
+                    {({ values, errors, touched }) =>
                         <Form >
                             <h1 className="text-xl font-bold text-dark-100 mb-5">Entrar no IFSolve</h1>
 
                             <div className="mb-3">
                                 <FormLabel label="Email ou usuário"></FormLabel>
                                 <FormControl name="login" placeholder="Digite seu email ou nome de usuário" />
+                                {errors.login && touched.login ? <div className="">{errors.login}</div> : null}
                             </div>
                             <div className="mb-3">
                                 <FormLabel label="Senha"></FormLabel>
@@ -47,8 +65,10 @@ export default function Login() {
                                         :
                                         null}
                                 </div>
-                            </div>
+                                {errors.password && touched.password ? <div className="">{errors.password}</div> : null}
 
+                            </div>
+                            {getError ? getError : null}
                             <button type="submit" className="w-full bg-primary-100 px-4 py-2 rounded-lg text-md font-medium text-dark-100 mb-5">Entrar</button>
                         </Form>
                     }
