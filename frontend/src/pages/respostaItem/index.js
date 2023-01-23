59508df0850dd49758031c2f3de2f5d0287b84cc
Livia-Rachell <liviarachelc@gmail.com>
2023-01-22 21:45:38 -0300

    visualização de resposta de um item (#82)

--- frontend/src/pages/respostaItem/index.js
+++ frontend/src/pages/respostaItem/index.js
@@ -1,10 +1,31 @@
-import { SidebarLayout } from "../../components"
+import { useEffect, useState } from "react";
+import { SidebarLayout, Container } from "../../components";
+import { GetRespostaByItem } from "../../api/config";
+import { useParams } from "react-router-dom";
 
 
 export default function RespostaItem() {
+    const [getItem, setItem] = useState([]);
+
+    let { id } = useParams();
+
+    useEffect(() => {
+        GetRespostaByItem(id)
+        .then(res =>{
+            setItem(res.data);
+        })
+    }, [id]);
+
     return (
         <SidebarLayout>
             <p>Respostas</p>
+            <br/>
+            {getItem.length > 0 && getItem.map((resp) => (
+                    <div>
+                        <p>{resp.resposta}</p><br/>
+                    </div>
+                ))
+            }
         </SidebarLayout>
     )
 }
