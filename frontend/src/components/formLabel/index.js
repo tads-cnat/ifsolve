27229a71e39dd19a10911867f412bbf09b0143f1
diogoodiego <diogo.sam.nascimento@gmail.com>
2023-04-12 15:04:23 -0300

    Nova configuração inicial (#138)

--- frontend/src/components/formLabel/index.js
+++
@@ -1,5 +0,0 @@
-export default function FormLabel(props) {
-    return (
-        <label className={"text-sm font-medium text-dark-100 " + props.className} htmlFor={props.label}>{props.label}</label>
-    )
-}
