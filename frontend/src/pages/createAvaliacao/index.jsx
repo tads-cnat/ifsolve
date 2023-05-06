3dde64502baf18104eafc6a9257f234a61e15515
Livia-Rachell <liviarachelc@gmail.com>
2023-05-06 16:12:46 -0300

    Adicionando type a botão (#140)

--- frontend/src/pages/createAvaliacao/index.jsx
+++ frontend/src/pages/createAvaliacao/index.jsx
@@ -287,7 +287,10 @@ export default function CreateAvaliacao() {
                     className="container flex flex-row mx-auto justify-start gap-4"
                     style={{ maxWidth: "720px" }}
                 >
-                    <button className="bg-primary-100 px-6 py-2 rounded-lg font-medium text-dark-100">
+                    <button
+                        type="submit"
+                        className="bg-primary-100 px-6 py-2 rounded-lg font-medium text-dark-100"
+                    >
                         Cadastrar
                     </button>
                     <button type="button" onClick={(e) => navigate(-1)}>
