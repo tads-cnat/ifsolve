221a535912fb1478609c40e2f1a458ba9d68ae2e
LIVIA RACHEL CHAVES DE MEDEIROS <l.rachel@academico.ifrn.edu.br>
2023-05-09 12:59:19 -0300

    Resolvendo erro jsx-a11y/label-has-associated-control da linha 194 (#140)

--- frontend/src/components/tagInput/index.jsx
+++ frontend/src/components/tagInput/index.jsx
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
-import { useState } from "react"
+import { useState } from "react";
 import { FiX } from "react-icons/fi";
 
 export default function TagInput({ get, set }) {
     const [getInput, setInput] = useState("");
 
     function handleChange(e) {
-        setInput(e.replace(',', ''));
+        setInput(e.replace(",", ""));
     }
     function handleKeyDown(e) {
         const trimInput = getInput.trim();
-        if ((e.key === "Enter") && trimInput.length > 0 && !get.includes(trimInput)) {
+        if (
+            e.key === "Enter" &&
+            trimInput.length > 0 &&
+            !get.includes(trimInput)
+        ) {
             e.preventDefault();
-            set(() => [...get, { 'nome': trimInput }]);
+            set(() => [...get, { nome: trimInput }]);
             setInput("");
         }
     }
@@ -24,14 +28,19 @@ export default function TagInput({ get, set }) {
     return (
         <div className="flex flex-col gap-2">
             <div className="flex gap-2 w-100 flex-wrap">
-                {get.map((tag) =>
-                    <div key={tag.id} className="flex gap-2 bg-dark-5 px-2 py-1 rounded-md">
-                        <small className="text-xs font-medium text-dark-100">{tag.nome}</small>
+                {get.map((tag) => (
+                    <div
+                        key={tag.id}
+                        className="flex gap-2 bg-dark-5 px-2 py-1 rounded-md"
+                    >
+                        <small className="text-xs font-medium text-dark-100">
+                            {tag.nome}
+                        </small>
                         <button
                             type="button"
                             onClick={(e) => removeTag(e, tag.nome)}
                             onKeyDown={(e) => {
-                                if (e.key === 'Enter' || e.key === ' ') {
+                                if (e.key === "Enter" || e.key === " ") {
                                     removeTag(e, tag.nome);
                                 }
                             }}
@@ -40,11 +49,21 @@ export default function TagInput({ get, set }) {
                             <FiX />
                         </button>
                     </div>
-                )}
+                ))}
             </div>
-            <input type="text" value={getInput} placeholder="Enter a tag" onKeyDown={(e) => handleKeyDown(e)} onChange={(e) => handleChange(e.target.value)} className="w-full px-6 py-4 bg-dark-5 rounded-lg" />
+            <label htmlFor="tags">
+                Tags
+                <input
+                    type="text"
+                    value={getInput}
+                    placeholder="Enter a tag"
+                    onKeyDown={(e) => handleKeyDown(e)}
+                    onChange={(e) => handleChange(e.target.value)}
+                    className="w-full px-6 py-4 bg-dark-5 rounded-lg"
+                />
+            </label>
         </div>
-    )
+    );
 }
 
 TagInput.propTypes = {
