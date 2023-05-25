1ab7794571a9d185971b87f6639e006ac1f2eea6
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2023-05-25 11:45:29 -0300

    Implementa testes de menor e maior para o model Area. (#208)

--- backend/config/ifsolve/tests/test_model_area.py
+++ backend/config/ifsolve/tests/test_model_area.py
@@ -42,3 +42,29 @@ class AreaTestCase(TestCase):
             descricao="O português é uma língua falada por milhões de pessoas ao redor do mundo. É uma língua românica, derivada do latim, assim como o espanhol, o francês e o italiano. "
             )
         self.assertEqual(area1, area2)
+
+    def test_area_com_nome_maior_que_o_nome_da_outra_area(self):
+        area1 = Area(
+            codigo="AR03",
+            nome="Ciência",
+            descricao="A Ciência abrange diversas áreas, como a Física, Química, Biologia, Astronomia, Geologia e muitas outras"
+            )
+        area2 = Area(
+            codigo="AR04",
+            nome="História",
+            descricao="História nos permite analisar criticamente diferentes pontos de vista, interpretar fontes históricas, desenvolver habilidades de pesquisa e análise, e aprender com os erros e sucessos do passado para construir um futuro melhor. "
+            )
+        self.assertGreater(area1, area2)
+
+    def test_area_com_nome_menor_que_o_nome_da_outra_area(self):
+        area1 = Area(
+            codigo="AR03",
+            nome="Ciência",
+            descricao="A Ciência abrange diversas áreas, como a Física, Química, Biologia, Astronomia, Geologia e muitas outras"
+            )
+        area2 = Area(
+            codigo="AR04",
+            nome="História",
+            descricao="História nos permite analisar criticamente diferentes pontos de vista, interpretar fontes históricas, desenvolver habilidades de pesquisa e análise, e aprender com os erros e sucessos do passado para construir um futuro melhor. "
+            )
+        self.assertLess(area2, area1)
