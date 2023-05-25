22ee5b16de269a7846c09cd0ee53e029d5987ae7
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2023-05-25 17:25:06 -0300

    Corrige testes de models Tag e Area para obter exito(#208)

--- backend/config/ifsolve/tests/test_model_area.py
+++ backend/config/ifsolve/tests/test_model_area.py
@@ -17,6 +17,19 @@ class AreaTestCase(TestCase):
             )
         self.assertEqual(area1, area2)
 
+    def test_area_com_nomes_e_codigos_diferentes(self):
+        area1 = Area(
+            codigo="AR01",
+            nome="Matemática",
+            descricao="A matemática é uma área do conhecimento que lida com números, formas, quantidades e padrões."
+            )
+        area2 = Area(
+            codigo="AR02",
+            nome="Português",
+            descricao="O português é uma língua falada por milhões de pessoas ao redor do mundo. É uma língua românica, derivada do latim, assim como o espanhol, o francês e o italiano. "
+            )
+        self.assertNotEqual(area1, area2)
+
     def test_area_com_nomes_iguais_e_codigos_diferentes(self):
         area1 = Area(
             codigo="AR01",
@@ -28,7 +41,7 @@ class AreaTestCase(TestCase):
             nome="Matemática",
             descricao="O português é uma língua falada por milhões de pessoas ao redor do mundo. É uma língua românica, derivada do latim, assim como o espanhol, o francês e o italiano. "
             )
-        self.assertEqual(area1, area2)
+        self.assertNotEqual(area1, area2)
 
     def test_area_com_nomes_diferentes_e_codigos_iguais(self):
         area1 = Area(
@@ -41,7 +54,7 @@ class AreaTestCase(TestCase):
             nome="Português",
             descricao="O português é uma língua falada por milhões de pessoas ao redor do mundo. É uma língua românica, derivada do latim, assim como o espanhol, o francês e o italiano. "
             )
-        self.assertEqual(area1, area2)
+        self.assertNotEqual(area1, area2)
 
     def test_area_com_nome_maior_que_o_nome_da_outra_area(self):
         area1 = Area(
@@ -54,7 +67,7 @@ class AreaTestCase(TestCase):
             nome="História",
             descricao="História nos permite analisar criticamente diferentes pontos de vista, interpretar fontes históricas, desenvolver habilidades de pesquisa e análise, e aprender com os erros e sucessos do passado para construir um futuro melhor. "
             )
-        self.assertGreater(area1, area2)
+        self.assertGreater(area2, area1)
 
     def test_area_com_nome_menor_que_o_nome_da_outra_area(self):
         area1 = Area(
@@ -67,4 +80,4 @@ class AreaTestCase(TestCase):
             nome="História",
             descricao="História nos permite analisar criticamente diferentes pontos de vista, interpretar fontes históricas, desenvolver habilidades de pesquisa e análise, e aprender com os erros e sucessos do passado para construir um futuro melhor. "
             )
-        self.assertLess(area2, area1)
+        self.assertLess(area1, area2)
--- backend/config/ifsolve/tests/test_model_tag.py
+++ backend/config/ifsolve/tests/test_model_tag.py
@@ -9,12 +9,17 @@ class TagTestCase(TestCase):
         tag2 = Tag(nome="Logaritmo")
         self.assertEqual(tag1, tag2)
 
+    def test_tags_com_nomes_diferentes(self):
+        tag1 = Tag(nome="Logarítmo")
+        tag2 = Tag(nome="Logaritmo")
+        self.assertNotEqual(tag1, tag2)
+
     def test_tag_menor_que_outra(self):
-        tag1 = Tag(nome="Logaritmo")
-        tag2 = Tag(nome="Algoritmo")
+        tag1 = Tag(nome="Algoritmo")
+        tag2 = Tag(nome="Logaritmo")
         self.assertLess(tag1, tag2)
 
     def test_tag_maior_que_outra(self):
-        tag1 = Tag(nome="Algoritmo")
-        tag2 = Tag(nome="Logaritmo")
+        tag1 = Tag(nome="Logaritmo")
+        tag2 = Tag(nome="Algoritmo")
         self.assertGreater(tag1, tag2)
