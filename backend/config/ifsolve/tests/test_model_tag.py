cae7a5d52ef6d25e04618c2c196b94762a3f3576
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2023-05-25 10:39:39 -0300

    Teste unitário area com código e nomes iguais. (#208)

--- backend/config/ifsolve/tests/test_model_tag.py
+++ backend/config/ifsolve/tests/test_model_tag.py
@@ -1,10 +1,51 @@
 from django.test import TestCase
-from ifsolve.models import Tag
+from ifsolve.models import Tag, Area
 
 
 class TagTestCase(TestCase):
 
     def test_tags_com_nomes_iguais(self):
-        tag1 = Tag(nome="Logarítmo")
+        tag1 = Tag(nome="Logaritmo")
         tag2 = Tag(nome="Logaritmo")
         self.assertEqual(tag1, tag2)
+
+class AreaTestCase(TestCase):
+
+    def test_area_com_nomes_e_codigos_iguais(self):
+        area1 = Area(
+            codigo="AR01",
+            nome="Matemática",
+            descricao="A matemática é uma área do conhecimento que lida com números, formas, quantidades e padrões."
+            )
+        area2 = Area(
+            codigo="AR01",
+            nome="Matemática",
+            descricao="O português é uma língua falada por milhões de pessoas ao redor do mundo. É uma língua românica, derivada do latim, assim como o espanhol, o francês e o italiano. "
+            )
+        self.assertEqual(area1, area2)
+
+    def test_area_com_nomes_iguais_e_codigos_diferentes(self):
+        area1 = Area(
+            codigo="AR01",
+            nome="Matemática",
+            descricao="A matemática é uma área do conhecimento que lida com números, formas, quantidades e padrões."
+            )
+        area2 = Area(
+            codigo="AR02",
+            nome="Matemática",
+            descricao="O português é uma língua falada por milhões de pessoas ao redor do mundo. É uma língua românica, derivada do latim, assim como o espanhol, o francês e o italiano. "
+            )
+        self.assertEqual(area1, area2)
+
+    def test_area_com_nomes_diferentes_e_codigos_iguais(self):
+        area1 = Area(
+            codigo="AR01",
+            nome="Matemática",
+            descricao="A matemática é uma área do conhecimento que lida com números, formas, quantidades e padrões."
+            )
+        area2 = Area(
+            codigo="AR01",
+            nome="Português",
+            descricao="O português é uma língua falada por milhões de pessoas ao redor do mundo. É uma língua românica, derivada do latim, assim como o espanhol, o francês e o italiano. "
+            )
+        self.assertEqual(area1, area2)
