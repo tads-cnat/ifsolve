aa2ffb98dba5ab44b4982be8f800a641b6fe501e
diogoodiego <diogo.sam.nascimento@gmail.com>
2023-06-03 18:34:51 -0300

    Estilização da tela de login (#153)

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/jsx-no-bind */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import IFSolvelogo from '../../images/IFSolve-logo.svg';
 import IFRNLogo from '../../images/ifrn-logo.png';
-// import LoginBackground from '../../images/login-background.png';
+import LoginBackground from '../../images/login-background.png';
 import { loginApi } from '../../api/config';
 import { InputPassword, InputText } from '../../components';
+import { FiLoader } from 'react-icons/fi';
 // import logoSuap from '../../images/logo-suap.svg';
 
 const schema = yup
@@ -19,6 +20,8 @@ const schema = yup
     .required();
 
 export default function Login() {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const {
         register,
@@ -27,14 +30,20 @@ export default function Login() {
     } = useForm({ resolver: yupResolver(schema) });
 
     function onSubmit(data) {
+        setLoading(true);
         loginApi(data)
             .then((res) => {
                 localStorage.setItem('ifsolve_token', res.data.token);
                 localStorage.setItem('ifsolve_user', JSON.stringify(res.data));
                 navigate('/avaliacao');
+                setLoading(false);
             })
-            .catch((error) => {
-                console.log(error);
+            .catch((err) => {
+                console.log(err);
+                setLoading(false);
+                setError(
+                    'Não foi possível fazer login. Verifique se digitou corretamente sua Matrícula e senha'
+                );
             });
     }
 
@@ -48,38 +57,73 @@ export default function Login() {
     }, []);
 
     return (
-        <div className="flex flex-col w-full h-screen justify-center items-center">
-            <form
-                onSubmit={handleSubmit(onSubmit)}
-                className="flex flex-col items-center w-full md:w-1/2 lg:w-1/3 p-8"
-            >
-                <img src={IFRNLogo} className="mb-12 h-8" alt="" />
-                <img src={IFSolvelogo} className="mb-12 h-8" alt="" />
-                <h3 className="text-lg font-semibold">Entre com sua matrícula do SUAP</h3>
-                <div className="mb-6 w-full">
+        <div className="grid grid-cols-1 lg:grid-cols-12 w-full h-screen">
+            <div className="lg:col-span-5 xl:col-span-4 p-8 flex flex-col justify-center bg-white">
+                <div className="flex gap-8 mb-12 ">
+                    <img src={IFSolvelogo} className="h-6" alt="" />
+                    <img src={IFRNLogo} className="h-6" alt="" />
+                </div>
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
                     <InputText
                         label="Usuário"
                         placeholder="Digite sua matrícula"
                         {...register('login')}
                     />
                     <p>{errors.login?.message}</p>
-                </div>
-                <div className="mb-6 w-full">
                     <InputPassword
                         label="Senha"
                         placeholder="Digite sua senha"
                         {...register('password')}
                     />
                     <p>{errors.password?.message}</p>
-                </div>
-
-                <button
-                    type="submit"
-                    className="w-full text-white font-semibold px-6 py-3 rounded-lg bg-emerald-600 hover:bg-emerald-500 focus:outline focus:outline-4 focus:outline-emerald-700"
-                >
-                    Login
-                </button>
-            </form>
+                    {error ? <p>{error}</p> : null}
+                    <button
+                        type="submit"
+                        className="w-full flex justify-center text-white font-semibold px-6 py-3 rounded-lg bg-emerald-600 hover:bg-emerald-500 focus:outline focus:outline-4 focus:outline-emerald-700"
+                    >
+                        {loading ? <FiLoader className="animate-spin text-xl" /> : 'Login'}
+                    </button>
+                </form>
+            </div>
+            {/* Background */}
+            <div className="hidden lg:block lg:col-span-7 xl:col-span-8 bg-emerald-200 h-screen">
+                <img src={LoginBackground} className="w-full h-full object-cover" alt="" />
+            </div>
         </div>
+        // <div className="flex flex-col w-full h-screen justify-center items-center">
+        //     <form
+        //         onSubmit={handleSubmit(onSubmit)}
+        //         className="flex flex-col items-center w-full md:w-1/2 lg:w-1/3 p-8"
+        //     >
+        //         <div className="flex gap-2">
+        //             <img src={IFSolvelogo} className="mb-12 h-6" alt="" />
+        //             <img src={IFRNLogo} className="mb-12 h-6" alt="" />
+        //         </div>
+        //         <h3 className="text-lg font-semibold">Entre com sua matrícula do SUAP</h3>
+        //         <div className="mb-6 w-full">
+        //             <InputText
+        //                 label="Usuário"
+        //                 placeholder="Digite sua matrícula"
+        //                 {...register('login')}
+        //             />
+        //             <p>{errors.login?.message}</p>
+        //         </div>
+        //         <div className="mb-6 w-full">
+        //             <InputPassword
+        //                 label="Senha"
+        //                 placeholder="Digite sua senha"
+        //                 {...register('password')}
+        //             />
+        //             <p>{errors.password?.message}</p>
+        //         </div>
+        //         {error ? <p>{error}</p> : null}
+        //         <button
+        //             type="submit"
+        //             className="w-full flex justify-center text-white font-semibold px-6 py-3 rounded-lg bg-emerald-600 hover:bg-emerald-500 focus:outline focus:outline-4 focus:outline-emerald-700"
+        //         >
+        //             {loading ? <FiLoader className="animate-spin text-xl" /> : 'Login'}
+        //         </button>
+        //     </form>
+        // </div>
     );
 }
