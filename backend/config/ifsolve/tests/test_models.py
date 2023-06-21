96c09b7901994809acf5fc7d48a5c65ed77a5815
LIVIA RACHEL CHAVES DE MEDEIROS <l.rachel@academico.ifrn.edu.br>
2023-06-21 12:10:17 -0300

    feat: Implementa teste de modelo Elaborador(#228)

---
+++ backend/config/ifsolve/tests/test_models.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from ifsolve.models import Elaborador
+from datetime import datetime
+
+# Create your tests here.
+
+class ElaboradorModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username='elaboradora',
+            password='el4b0r4d0r4',
+            email='elaboradora@example.com'
+        )
+
+        self.elaborador = Elaborador.objects.create(
+            user=self.user,
+            nome_completo='Elaboradora da Silva',
+            data_nascimento='2000-06-01',
+            verificado=True
+        )
+
+    def test_user_elaborador_creation(self):
+        self.assertEqual(self.user.username, 'elaboradora')
+        self.assertEqual(self.user.password, 'el4b0r4d0r4')
+        self.assertEqual(self.user.email, 'elaboradora@example.com')   
+        self.assertEqual(self.elaborador.user, self.user)
+        self.assertEqual(self.elaborador.nome_completo, 'Elaboradora da Silva')
+        self.assertEqual(self.elaborador.data_nascimento, '2000-06-01')
+        self.assertEqual(self.elaborador.verificado, True)
+
+    def test_elaborador_edit(self):
+        self.user.username = 'elaboradora_edit'
+        self.user.password = 'el4b0r4d0r4edit'
+        self.user.email = 'elaboradora_edit@example.com'
+        self.elaborador.nome_completo = 'Elaboradora Edit da Silva'
+        self.elaborador.data_nascimento = '2002-06-01'
+        self.elaborador.verificado = False
+        self.user.save()
+        self.elaborador.save()
+
+        update_user = User.objects.get(id=self.user.pk)
+        update_elaborador = Elaborador.objects.get(id=self.elaborador.pk)
+        self.assertEqual(update_user.username, 'elaboradora_edit')
+        self.assertEqual(update_user.password, 'el4b0r4d0r4edit')
+        self.assertEqual(update_user.email, 'elaboradora_edit@example.com')
+        self.assertEqual(update_elaborador.nome_completo, 'Elaboradora Edit da Silva')
+        self.assertEqual(update_elaborador.data_nascimento, datetime.strptime('2002-06-01', '%Y-%m-%d').date())
+        self.assertEqual(update_elaborador.verificado, False)
+
+    def test_elaborador_listing(self):
+        user2 = User.objects.create(
+            username='teste',
+            password='t3st3',
+            email='teste@example.com'
+        )
+
+        Elaborador.objects.create(
+            user=user2,
+            nome_completo='Teste da Silva',
+            data_nascimento='2000-06-01',
+            verificado=True
+        )
+
+        elaboradores = Elaborador.objects.all()
+        self.assertEqual(elaboradores.count(), 2)  # Contando com o item criado no setUp
+
+    def test_elaborador_deletion(self):
+        self.user.delete()
+        self.elaborador.delete()
+
+        usuarios = User.objects.all()
+        self.assertEqual(usuarios.count(), 0)
+
+        elaboradores = Elaborador.objects.all()
+        self.assertEqual(elaboradores.count(), 0)
