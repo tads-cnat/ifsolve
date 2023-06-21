4f69d7b4734d16e723a2112de6ef225dd0018599
Raphael Matias <r.avelino@escolar.ifrn.edu.br>
2023-06-21 13:59:32 -0300

    Corrigindo teste de resposta #226

--- backend/config/ifsolve/tests/test_models.py
+++ backend/config/ifsolve/tests/test_models.py
@@ -2,19 +2,17 @@ import unittest
 from django.test import TestCase
 from datetime import date
 from django.contrib.auth.models import User
-from myapp.models import Usuario, Aluno, Alternativa, Item, ItemAvaliacao
+from ifsolve.models import Usuario, Aluno, Alternativa, Item, ItemAvaliacao, Resposta
+
 
 class ModelsIntegrationTest(TestCase):
     def setUp(self):
+
         self.user = User.objects.create_user(
             username='testuser',
             password='testpassword'
         )
-        self.usuario = Usuario.objects.create(
-            user=self.user,
-            nome_completo='Fulano de Tal',
-            data_nascimento=date(1990, 1, 1)
-        )
+
         self.aluno = Aluno.objects.create(
             user=self.user,
             nome_completo='Beltrano de Tal',
@@ -43,20 +41,55 @@ class ModelsIntegrationTest(TestCase):
             nota_item=10
         )
 
-    def test_usuario_str(self):
-        self.assertEqual(str(self.usuario), 'testuser')
+        self.resposta = Resposta.objects.create(
+            aluno=self.aluno,
+            item=self.item,
+            item_avaliacao=self.item_avaliacao,
+            resposta='a',
+            nota_obtida=10,
+            data_hora=date.today()
+        )
 
-    def test_aluno_str(self):
-        self.assertEqual(str(self.aluno), 'testuser')
+    def test_resposta_creation(self):
+        test_resposta = Resposta.objects.get(
+            aluno=self.aluno, item=self.item, item_avaliacao=self.item_avaliacao, resposta='a', nota_obtida=10, data_hora=date.today())
+        self.assertEqual(test_resposta, self.resposta)
+        self.assertEqual(test_resposta.aluno, self.aluno)
+        self.assertEqual(test_resposta.item, self.item)
+        self.assertEqual(test_resposta.item_avaliacao, self.item_avaliacao)
+        self.assertEqual(test_resposta.resposta, 'a')
+        self.assertEqual(test_resposta.nota_obtida, 10)
+        self.assertEqual(test_resposta.data_hora, date.today())
 
-    def test_alternativa_str(self):
-        self.assertEqual(str(self.alternativa), 'Texto da Alternativa')
+    def test_resposta_update(self):
+        test_resposta = Resposta.objects.get(
+            aluno=self.aluno, item=self.item, item_avaliacao=self.item_avaliacao, resposta='a', nota_obtida=10, data_hora=date.today())
+        test_resposta.resposta = 'b'
+        test_resposta.nota_obtida = 5
+        test_resposta.data_hora = date(2020, 1, 1)
+        test_resposta.save()
 
-    def test_item_str(self):
-        self.assertEqual(str(self.item), 'Título do Item')
+        self.assertEqual(test_resposta.resposta, 'b')
+        self.assertEqual(test_resposta.nota_obtida, 5)
+        self.assertEqual(test_resposta.data_hora, date(2020, 1, 1))
 
-    def test_item_avaliacao_str(self):
-        self.assertEqual(str(self.item_avaliacao), '1 - Título do Item')
+    def test_resposta_list(self):
+        test_resposta = Resposta.objects.all()
+        self.assertEqual(test_resposta.count(), 1)
+        self.assertEqual(test_resposta[0].aluno, self.aluno)
+        self.assertEqual(test_resposta[0].item, self.item)
+        self.assertEqual(test_resposta[0].item_avaliacao, self.item_avaliacao)
+        self.assertEqual(test_resposta[0].resposta, 'a')
+        self.assertEqual(test_resposta[0].nota_obtida, 10)
+        self.assertEqual(test_resposta[0].data_hora, date.today())
+
+    def test_resposta_delete(self):
+        test_resposta = Resposta.objects.get(
+            aluno=self.aluno, item=self.item, item_avaliacao=self.item_avaliacao, resposta='a', nota_obtida=10, data_hora=date.today())
+        test_resposta.delete()
+        test_resposta = Resposta.objects.all()
+        self.assertEqual(test_resposta.count(), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
