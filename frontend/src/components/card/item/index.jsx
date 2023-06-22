ba5cd4d5572840b443067988a99a1e2ea7102763
diogoodiego <diogo.sam.nascimento@gmail.com>
2023-06-22 00:12:20 -0300

    Ajuste no card de items (#153)

--- frontend/src/components/card/item/index.jsx
+++ frontend/src/components/card/item/index.jsx
@@ -3,25 +3,21 @@ import { FiCheckSquare, FiAlignLeft } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
 
 export default function CardItem({ data }) {
-
     const navigate = useNavigate();
     const { id, tipo, assunto, titulo, tags } = data;
 
     function handleButtonClick(buttonId) {
-        if (buttonId === "responder") {
-            console.log("Resposder")
+        if (buttonId === 'responder') {
+            console.log('Resposder');
             navigate(`/item/${id}`);
-
-        } else if (buttonId === "minhas-respostas") {
-            console.log("Minhas respostas")
+        } else if (buttonId === 'minhas-respostas') {
+            console.log('Minhas respostas');
             navigate(`/resposta/item/${id}`);
         }
     }
 
     return (
-        <div
-            className="group flex flex-col p-4 gap-2 bg-white rounded-lg transition-all duration-200 linear outline outline-0 outline-dark-40"
-        >
+        <div className="group flex flex-col p-4 gap-2 bg-white rounded-lg transition-all duration-200 linear outline outline-0 outline-dark-40">
             <div className="flex flex-row justify-between items-center mb-4">
                 <span className="text-2xl bg-amber-100 p-2 rounded-lg text-amber-600 ">
                     {tipo === 'ME' ? <FiCheckSquare /> : <FiAlignLeft />}
@@ -29,7 +25,7 @@ export default function CardItem({ data }) {
             </div>
             <h4 className="text-lg font-medium text-dark-80">{titulo}</h4>
             <p className="text-dark-60">{assunto}</p>
-            <div className="flex flex-row justify-between items-center mb-2">
+            <div className="flex flex-row gap-2 flex-nowrap overflow-x-hidden items-center mb-2">
                 {tags.map((tag) => (
                     <span
                         className="text-sm px-2 py-1 bg-amber-100 text-amber-600 rounded-lg"
@@ -40,9 +36,21 @@ export default function CardItem({ data }) {
                 ))}
             </div>
             <div className="flex flex-row justify-between items-center mb-2">
-                <button type='button' onClick={() => handleButtonClick("responder")} className="text-white border border-amber-600 hover:text-white bg-amber-600 hover:bg-amber-700 hover:border-amber-700 font-medium rounded-lg text-sm px-5 py-2.5 mr-2">Responder</button>
+                <button
+                    type="button"
+                    onClick={() => handleButtonClick('responder')}
+                    className="text-white border border-amber-600 hover:text-white bg-amber-600 hover:bg-amber-700 hover:border-amber-700 font-medium rounded-lg text-sm px-5 py-2.5 mr-2"
+                >
+                    Responder
+                </button>
 
-                <button type="button" onClick={() => handleButtonClick("minhas-respostas")} className="text-amber-600 bg-white border border-amber-200  hover:bg-amber-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2">Minhas respostas</button>
+                <button
+                    type="button"
+                    onClick={() => handleButtonClick('minhas-respostas')}
+                    className="text-amber-600 bg-white border border-amber-200  hover:bg-amber-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2"
+                >
+                    Minhas respostas
+                </button>
             </div>
         </div>
     );
