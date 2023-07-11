51fcd15512c1dc9d0d80e65550b6dc7ab06cbb24
LIVIA RACHEL CHAVES DE MEDEIROS <l.rachel@academico.ifrn.edu.br>
2023-07-11 12:13:58 -0300

    feat: Implementa apitestcase getAvaliacao(#241)

--- backend/config/ifsolve/tests/test_api/test_avaliacao.py
+++ backend/config/ifsolve/tests/test_api/test_avaliacao.py
@@ -1,18 +1,37 @@
-import requests
 import pytest
+from ifsolve.models import Elaborador
+from rest_framework import status
+from django.contrib.auth.models import User
+from datetime import date
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIClient
 
 BASE_URL = "http://localhost:8000"
 
-def test_authenticated_route():
-    headers = {
-        "Authorization": "Token 56308bd1b840cf3ac0fbd09bf7a83e9aa19af08d"
-    }
-    
-    route_url = BASE_URL + "/avaliacao/"
-    
-    response = requests.get(route_url, headers=headers)
-    
-    assert response.status_code == 200
+class ItemTestsSistema(APITestCase):
+    def setUp(self):
+        self.user_elaborador = User.objects.create_superuser(
+                    username='testuser',
+                    password='testpassword'
+                )
+
+        self.elaborador = Elaborador.objects.create(
+            user=self.user_elaborador,
+            verificado=True,
+            nome_completo='Test User',
+            data_nascimento=date.today()
+        )
+
+        self.token = Token.objects.create(user=self.user_elaborador)
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user_elaborador)
+
+
+    def test_authenticated_route_1(self):
+        route_url = BASE_URL + "/avaliacao/"
+        response = self.client.get(route_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+            
 
 if __name__ == "__main__":
     pytest.main()
