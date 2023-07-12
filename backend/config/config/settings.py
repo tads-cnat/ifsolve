c3deed231a4410a37b9ad8e3cb41d0fd4e834643
diogoodiego <diogo.sam.nascimento@gmail.com>
2023-07-12 14:05:38 -0300

    Atualizando compose (#239)

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'postgresql',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db_test.sqlite3',
     }
 }
 
